698ddc7f3d2a66b2542b188070d443b6f1b68344
mrishikesh07 <rishikeshmasurkar7@gmail.com>
2026-03-09 17:15:52 +0530

    Display Task Logicand rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react"
 import Input from "./components/Input"
+import DisplayBoard from "./components/DisplayBoard";
 
 
 function App() {
   const [allTask, setAllTasks] = useState([]);
 
   const addTask = (task) =>{
-    setAllTasks(prev => [...prev, task]);
+    const newTask = {
+      id: Date.now(),
+      text: task
+    }
+    setAllTasks(prev => [...prev, newTask]);
   }
 
   console.log(allTask);
   return (
     <>
       <Input onAdd={addTask}/>
+      <div>
+        {
+          allTask.map((task)=>
+            <DisplayBoard key={task.id} task={task}/>
+          )
+        }
+      </div>
     </>
   )
 }
--- src/components/DisplayBoard.jsx
+++ src/components/DisplayBoard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const DisplayBoard = () => {
+const DisplayBoard = ({task}) => {
   return (
     <div>
-        <p></p>
+        <p>{task.text}</p>
     </div>
   )
 }
